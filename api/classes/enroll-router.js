53a3b0d2d0dbb0ae7cf99761f93a93e3013dfb7b
AdrianHDev <adrianhensondev@gmail.com>
2021-07-28 00:56:04 -0400

    wip

--- api/classes/enroll-router.js
+++ api/classes/enroll-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Classes = require('./classes-model')
 
 const router = express.Router();
 
@@ -6,15 +7,28 @@ const router = express.Router();
 // attendants table only needs the user_id and the class_id
 
 
-router.get('/:class_id', (req, res, next) => {
+router.get('/:class_id', async (req, res, next) => {
+    try {
+        const result = await Classes.getById(req.params.class_id, req.token.user_id);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json(error)
+    }
 
 });
 
+// Not sure what I'm supposed to be doing here.
 router.post('/:class_id', (req, res, next) => {
 
 });
 
-router.delete('/:class_id', (req, res, next) => {
+router.delete('/:class_id', async (req, res, next) => {
+    try {
+        const removed = await Classes.remove(req.params.class_id);
+        res.json(removed)
+    } catch (err) {
+        console.error(err)
+    }
 
 });
 
